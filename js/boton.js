1bceef13aa59d04787aae832e762a117b20199ea
Francisco Nicolas Hernandorena <fnhernandorena@gmail.com>
2023-11-11 00:45:28 -0300

    division de gastos

--- js/boton.js
+++ js/boton.js
@@ -1,15 +1,17 @@
+'use strict';
+
 const boton = document.getElementById("boton");
 const naveg = document.getElementById("naveg");
 const btnClose = document.getElementById("btn-close");
 boton.addEventListener("click", () => {
-    naveg.style.visibility = "visible"
-    btnClose.style.visibility = "visible"
-    boton.style.visibility = "hidden"
+    naveg.style.visibility = "visible";
+    btnClose.style.visibility = "visible";
+    boton.style.visibility = "hidden";
 });
 
 btnClose.addEventListener("click", () => {
-    naveg.style.visibility = "hidden"
-    btnClose.style.visibility = "hidden"
-    boton.style.visibility = "visible"   
+    naveg.style.visibility = "hidden";
+    btnClose.style.visibility = "hidden";
+    boton.style.visibility = "visible";   
 });
 
